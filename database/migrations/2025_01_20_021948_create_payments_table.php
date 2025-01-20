e8c26903ea1b026a3d4d1f3380e02f3c197f4d8b
Ar536 <ariqmuhammadfathin2@gmail.com>
2025-01-20 10:44:23 +0700

    feat: payment_method

--- database/migrations/2025_01_20_021948_create_payments_table.php
+++ database/migrations/2025_01_20_021948_create_payments_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->string('payment_code')->nullable();
+            $table->unsignedBigInteger('booking_id')->nullable();
+            $table->unsignedBigInteger('payment_method_id')->nullable();
+            $table->decimal('amount', 10, 2);
+            $table->date('payment_date');
+            $table->enum('status', ['pending', 'confirmed', 'failed']);
             $table->timestamps();
         });
     }
--- database/migrations/2025_01_20_022246_create_bookings_table.php
+++ database/migrations/2025_01_20_022246_create_bookings_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('schedule_id');
+            $table->integer('quantity');
+            $table->date('booking_date');
             $table->timestamps();
         });
     }
