aa22d426972952a97e77acf3580bab1ec6b81b84
hiphipdora <faaay.adistiii@gmail.com>
2025-01-25 00:17:17 +0700

    yea

--- app/Http/Controllers/Api/ScheduleController.php
+++ app/Http/Controllers/Api/ScheduleController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
-use App\Models\schedule;
+use App\Models\Schedule;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
