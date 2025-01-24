e4551436b77be5b4ff1b9b3728e0370f6134b405
FarhanGamal <farhangamal78@gmail.com>
2025-01-24 14:03:51 +0700

    fix: autentication

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 
-<<<<<<< HEAD
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
@@ -39,17 +38,8 @@ Route::middleware(['auth:api'])->group(function () {
                 Route::apiResource('/movies', MovieController::class)->only(['store', 'update', 'destroy']);
                 Route::apiResource('/payments', PaymentController::class);
                 Route::apiResource('/payment_methods',PaymentMethodController::class);
+                Route::apiResource('/genre',GenreController::class);
 
             });
 
 });
-=======
-Route::apiResource('/genres', GenreController::class);
-Route::apiResource('/payment_methods',PaymentMethodController::class);
-Route::apiResource('/studios',StudioController::class);
-Route::apiResource('/seats',SeatController::class);
-Route::apiResource('/movies', MovieController::class);
-Route::apiResource('/schedules', ScheduleController::class);
-Route::apiResource('/payments', PaymentController::class);
-Route::apiResource('/bookings', BookingController::class);
->>>>>>> d0fcdae3452e934f561dc485758d7a45456edb14
