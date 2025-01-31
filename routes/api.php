2b5e5834a0ec6a1189bb93d202c9ae85367e961b
hiphipdora <faaay.adistiii@gmail.com>
2025-01-31 15:25:12 +0700

    fix: route api schedule

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ Route::middleware(['auth:api'])->group(function () {
             // Route::apiResource('/payment_methods',PaymentMethodController::class);
             // Route::apiResource('/studios',StudioController::class);
 
-            Route::apiResource('/schedules', ScheduleController::class);
+            Route::apiResource('/schedules', ScheduleController::class)->only(['store', 'update', 'destroy']);
             Route::apiResource('/bookings', BookingController::class)->only(['index', 'show']);
             Route::apiResource('/booking', BookingController::class);
             Route::apiResource('/payments', PaymentController::class)->only(['index', 'store']);
