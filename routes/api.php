76a9fcfa3a600ac9ea978ae576d54d00c7f88167
FarhanGamal <farhangamal78@gmail.com>
2025-02-03 14:05:33 +0700

    tes

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,6 @@ Route::apiResource('/payment_methods',PaymentMethodController::class)->only(['in
 Route::apiResource('/schedules', ScheduleController::class)->only(['index', 'show' ]);
 Route::apiResource('/seats',SeatController::class)->only(['index', 'show' ]);
 Route::apiResource('/schedules', ScheduleController::class)->only(['index', 'show']);
-Route::apiResource('/bookings', BookingController::class)->only(['index', 'show']);
 
 Route::middleware(['auth:api'])->group(function () {
     Route::get('/user', fn(Request $request) => $request->user());
@@ -35,6 +34,8 @@ Route::middleware(['auth:api'])->group(function () {
             // Route::apiResource('/genres', GenreController::class);
             // Route::apiResource('/payment_methods',PaymentMethodController::class);
             // Route::apiResource('/studios',StudioController::class);
+            Route::apiResource('/bookings', BookingController::class)->only(['index', 'show', 'store']);
+
 
 
 
