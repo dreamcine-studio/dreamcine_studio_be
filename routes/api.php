f1ea82434f8519b61924b03d65a117433ce8b4e4
Azhar_Fikri <azharfikri889@gmail.com>
2025-02-03 14:02:43 +0700

    movie and api

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,8 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api'
 Route::apiResource('/movies', MovieController::class)->only(['index', 'show']);
 Route::apiResource('/genres', GenreController::class)->only(['index', 'show']);
 Route::apiResource('/studios',StudioController::class)->only(['index', 'show']);
+Route::apiResource('/payments',PaymentController::class)->only(['index', 'show']);
+Route::apiResource('/schedules',ScheduleController::class)->only(['index', 'show']);
 Route::apiResource('/payment_methods',PaymentMethodController::class)->only(['index', 'show']);
 
 Route::middleware(['auth:api'])->group(function () {
@@ -31,7 +33,7 @@ Route::middleware(['auth:api'])->group(function () {
             // Route::apiResource('/genres', GenreController::class);
             // Route::apiResource('/payment_methods',PaymentMethodController::class);
             // Route::apiResource('/studios',StudioController::class);
-            Route::apiResource('/schedules', ScheduleController::class);
+            // Route::apiResource('/schedules', ScheduleController::class);
             Route::apiResource('/booking', BookingController::class);
 
 
