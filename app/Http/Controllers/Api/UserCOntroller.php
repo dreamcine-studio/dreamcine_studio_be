ec11c7bafa179993eda49e84d3eef2d2a5c4d417
Azhar_Fikri <azharfikri889@gmail.com>
2025-02-07 08:25:11 +0700

    feat : User Controller

---
+++ app/Http/Controllers/Api/UserCOntroller.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    public function index()
+    {
+        // mengambil data genre
+        $genres = User::all();
+
+        // mengecek data genre
+        if ($genres->isEmpty()) {
+            return response()->json([
+                "success" => true,
+                "message" => "Resource data not found!"
+            ], 200);
+        }
+
+        // memberi pesan berhasil
+        return response()->json([
+            "success" => true,
+            "message" => "Get All Resource",
+            "data" => $genres
+        ], 200);
+    }
+}
