51e1ed4b198f6611eba07aa24cdddb24ff771c82
hiphipdora <faaay.adistiii@gmail.com>
2025-02-10 09:34:05 +0700

    fix: movie and genre seeder

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -12,18 +12,31 @@ class GenreSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        Genre::create([
-            'name' => 'Action',
-            'description' => 'Movies that feature intense physical activity, including fights, chases, and explosions.',
-        ]);
-        Genre::create([
-            'name' => 'Romance',
-            'description' => 'Movies that explore love stories and emotional relationships between characters.',
-        ]);
-        Genre::create([
-            'name' => 'Horror',
-            'description' => 'Movies designed to scare and thrill audiences with suspenseful and frightening elements.',
-        ]);
-    }
-}                                                   
+{
+    Genre::create([
+        'name' => 'Action',
+        'description' => 'Movies that feature intense physical activity, including fights, chases, and explosions.',
+    ]);
+    Genre::create([
+        'name' => 'Romance',
+        'description' => 'Movies that explore love stories and emotional relationships between characters.',
+    ]);
+    Genre::create([
+        'name' => 'Horror',
+        'description' => 'Movies designed to scare and thrill audiences with suspenseful and frightening elements.',
+    ]);
+    Genre::create([
+        'name' => 'Comedy',
+        'description' => 'Movies intended to make audiences laugh through humor, satire, or amusing situations.',
+    ]);
+    Genre::create([
+        'name' => 'Drama',
+        'description' => 'Movies that focus on realistic storytelling, emotional themes, and character development.',
+    ]);
+    Genre::create([
+        'name' => 'Science Fiction',
+        'description' => 'Movies that explore futuristic concepts, advanced technology, space travel, and extraterrestrial life.',
+    ]);
+}
+
+}
