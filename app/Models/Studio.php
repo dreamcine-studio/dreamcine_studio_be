5e77f4bf4572dcf6b345e4fc906836e9df59f737
hiphipdora <faaay.adistiii@gmail.com>
2025-02-11 12:19:00 +0700

    fix: studio delete location

--- app/Models/Studio.php
+++ app/Models/Studio.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Studio extends Model
 {
     protected $fillable = [
-        'name', 'location', 'maxseats'
+        'name', 'maxseats'
     ];
 }
--- database/migrations/2025_01_20_022350_create_studios_table.php
+++ database/migrations/2025_01_20_022350_create_studios_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('studios', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('location');
             $table->integer('maxseats');
             $table->timestamps();
         });
